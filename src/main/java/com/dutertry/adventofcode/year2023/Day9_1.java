2bb3c373abbd34e785834fe9500aacba880251bc
Nicolas Dutertry <nicolas@dutertry.com>
2023-12-09 09:48:03 +0100

    Add day 9 year 2023

---
+++ src/main/java/com/dutertry/adventofcode/year2023/Day9_1.java
@@ -0,0 +1,54 @@
+package com.dutertry.adventofcode.year2023;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Day9_1 {
+    public static void main(String[] args) {
+
+        try (BufferedReader br = AdventUtils.getBufferedReader(9)) {
+            String line;
+            int total = 0;
+            while ((line = br.readLine()) != null) {
+                List<Integer> seq = Arrays.asList(StringUtils.split(line))
+                        .stream()
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toList());
+                List<List<Integer>> seqList = new LinkedList<List<Integer>>();
+                seqList.add(seq);
+                while (true) {
+                    List<Integer> nextSeq = new ArrayList<Integer>();
+                    for (int i = 1; i < seq.size(); i++) {
+                        nextSeq.add(seq.get(i) - seq.get(i - 1));
+                    }
+                    seq = nextSeq;
+                    seqList.add(seq);
+                    if (seq.stream().allMatch(i -> i == 0)) {
+                        break;
+                    }
+                }
+
+                int nextVal;
+                for (int i = seqList.size() - 1; i > 0; i--) {
+                    List<Integer> seqNext = seqList.get(i);
+                    seq = seqList.get(i - 1);
+                    nextVal = seq.get(seq.size() - 1) + seqNext.get(seqNext.size() - 1);
+                    seq.add(nextVal);
+                }
+
+                total += seq.get(seq.size() - 1);
+            }
+            System.out.println(total);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
