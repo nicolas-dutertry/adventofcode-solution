3df10ba7f798ed88c4b5d316526306b1adf21b46
Nicolas Dutertry <nicolas@dutertry.com>
2023-12-10 16:49:46 +0100

    Add days 10 year 2015

---
+++ src/main/java/com/dutertry/adventofcode/year2015/Day10_1.java
@@ -0,0 +1,36 @@
+package com.dutertry.adventofcode.year2015;
+
+public class Day10_1 {
+    public static void main(String[] args) {
+        String s = "1321131112";
+        for (int i = 0; i < 40; i++) {
+            s = say(s);
+        }
+        System.out.println(s.length());
+	}
+    
+    private static String say(String s) {
+        StringBuilder result = new StringBuilder();
+        char current = '-';
+        int currentCount = 0;
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if(i == 0) {
+                current = c;
+                currentCount = 1;
+            } else {
+                if(c == current) {
+                    currentCount++;
+                } else {
+                    result.append(currentCount);
+                    result.append(current);
+                    current = c;
+                    currentCount = 1;
+                }
+            }
+        }
+        result.append(currentCount);
+        result.append(current);
+        return result.toString();
+    }
+}
